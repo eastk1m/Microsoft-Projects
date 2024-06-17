86416a990008cd6ba04ee7665057f5b494bdabf5
eastk1m <168964532+eastk1m@users.noreply.github.com>
2024-06-18 00:09:29 +0900

    Add files via upload

---
+++ Week 3/3.1/3.1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Text;
+
+class Program
+{
+    static string GetEvenNumbers()
+    {
+        StringBuilder result = new StringBuilder();
+        for (int i = 2; i < 100; i += 2)
+        {
+            result.Append(i);
+            if (i < 98)
+                result.Append(", ");
+        }
+        return result.ToString();
+    }
+
+    static void Main()
+    {
+        string evenNumbers = GetEvenNumbers();
+        Console.WriteLine($"Even numbers between 0 and 100: {evenNumbers}");
+    }
+}
---
+++ Week 3/3.1D/3.1D/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _3._1D
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
