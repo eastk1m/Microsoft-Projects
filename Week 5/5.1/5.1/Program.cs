b02c06b7cb90d5cf85de77ccc409d95f4cefdab0
eastk1m <168964532+eastk1m@users.noreply.github.com>
2024-06-18 00:13:34 +0900

    Add files via upload

---
+++ Week 5/5.1/5.1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class Program
+{
+    static bool IsPalindrome(int x)
+    {
+        string numString = x.ToString();
+        int length = numString.Length;
+
+        for (int i = 0; i < length / 2; i++)
+        {
+            if (numString[i] != numString[length - 1 - i])
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    static void Main()
+    {
+        Console.Write("Enter a number to check if it's a palindrome: ");
+        int input = int.Parse(Console.ReadLine());
+
+        bool isPalindromic = IsPalindrome(input);
+
+        Console.WriteLine($"Is {input} a palindrome? {isPalindromic}");
+    }
+}
