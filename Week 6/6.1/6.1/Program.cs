9d63280e3d10c4db515d5d62048a752f44eb0742
eastk1m <168964532+eastk1m@users.noreply.github.com>
2024-06-18 00:17:23 +0900

    Add files via upload

---
+++ Week 6/6.1/6.1/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace _6._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+            {
+                // Create a linked list of strings
+                LinkedList<string> myList = new LinkedList<string>();
+                myList.AddLast("First house");
+                myList.AddLast("Second house");
+                // ... Add more houses
+
+                // Search for a house by its number (you'll need to adapt this for your data structure)
+                string searchNumber = "123"; // Example house number
+                var foundHouse = myList.Find(searchNumber);
+                if (foundHouse != null)
+                {
+                    Console.WriteLine($"Found house: {foundHouse.Value}");
+                    // Display other details (address, type) here
+                }
+                else
+                {
+                    Console.WriteLine($"House with number {searchNumber} not found.");
+                }
+            }
+        }
+
+
+    }
