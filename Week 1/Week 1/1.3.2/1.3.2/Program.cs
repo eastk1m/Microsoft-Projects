f181f39b714bd090971190591fb6b27b481640d5
eastk1m <168964532+eastk1m@users.noreply.github.com>
2024-06-17 23:22:08 +0900

    Add files via upload

---
+++ Week 1/Week 1/1.3.2/1.3.2/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace _1._3._2
+
+// C# to read n number of values in an array and display it in reverse order.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        { 
+            int[] array = {1, 2, 3 };
+            int n = array.Length;
+            int temp = 0;
+     
+            for (int i = 0; i < (n/2); i++)
+            {
+              
+                temp = array[i]; 
+                array[i] = array[n - i - 1];
+                array[(n - 1) - i] = temp;
+
+            }
+
+
+            foreach (int i in array)
+
+            {
+                Console.WriteLine(i);
+            }
+
+
+
+
+        }
+    }
+}
---
+++ Week 1/Week 1/1.4/1.4/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace _1._4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+
+        {
+           Point point1 = new Point();
+           Point point2 = new Point();
+
+            if (point1.x == point2.x)
+            {
+                Console.WriteLine("Points are on the same axis");
+            }
+            else
+
+            {
+                Console.WriteLine("Point not on the same axis");
+
+            }
+
+
+        }
+        struct Point
+        {
+           public double x;
+           public double y;
+
+        }
+
+
+
+            }
+}
+}
