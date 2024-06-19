6179d928f7f2ef3612405bf4b434c6cd1f679034
eastk1m <168964532+eastk1m@users.noreply.github.com>
2024-06-20 03:40:18 +0900

    Add files via upload

---
+++ Week 7/7.2B/7.2B/Program.cs
@@ -0,0 +1,56 @@
+﻿namespace _7._2B
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            static bool IsVowel(char c)
+            {
+                c = char.ToLower(c);
+                return c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u';
+            }
+
+            static string ReverseVowels(string s)
+            {
+                char[] charArray = s.ToCharArray();
+                int i = 0, j = s.Length - 1;
+
+                while (i < j)
+                {
+                    if (IsVowel(charArray[i]) && IsVowel(charArray[j]))
+                    {
+                        char temp = charArray[i];
+                        charArray[i] = charArray[j];
+                        charArray[j] = temp;
+                        i++;
+                        j--;
+                    }
+                    else if (IsVowel(charArray[i]))
+                    {
+                        j--;
+                    }
+                    else
+                    {
+                        i++;
+                    }
+                }
+
+                return new string(charArray);
+            }
+
+            static void Main()
+            {
+                string input1 = "hello";
+                string input2 = "avacado";
+                string input3 = "intelligent";
+
+                Console.WriteLine("Output for \"" + input1 + "\": " + ReverseVowels(input1));
+                Console.WriteLine("Output for \"" + input2 + "\": " + ReverseVowels(input2));
+                Console.WriteLine("Output for \"" + input3 + "\": " + ReverseVowels(input3));
+            }
+        }
+
+    }
+}
+
---
+++ Week 7/7.2C/7.2C/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace _7._2C
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+ 
+            static bool IsAnagram(string s, string t)
+            {
+                // Convert both strings to character arrays
+                char[] sChars = s.ToCharArray();
+                char[] tChars = t.ToCharArray();
+
+                // Sort the character arrays
+                Array.Sort(sChars);
+                Array.Sort(tChars);
+
+                // Compare the sorted arrays
+                return new string(sChars) == new string(tChars);
+            }
+
+            static void Main()
+            {
+                string s1 = "anagram";
+                string t1 = "nagaram";
+                string s2 = "rat";
+                string t2 = "car";
+
+                Console.WriteLine(IsAnagram(s1, t1)); // Output: True
+                Console.WriteLine(IsAnagram(s2, t2)); // Output: False
+            }
+        }
+
+    }
+}
+
